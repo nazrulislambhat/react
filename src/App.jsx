72938eea367bfead830a7eabbde47d67e3bc793a
Nazrul Islam <nazrulislambhat@gmail.com>
2024-06-25 11:06:57 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>Hello React + Vite</h1>
-    </>
-  );
+  // return (
+  //   <>
+  //     <h1 id="h1">Hello React + Vite</h1>
+  //   </>
+  // );
+  return React.createElement('h1', { id: 'h1111' }, 'Hello React + Vite');
 }
 
 export default App;
