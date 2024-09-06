3bab8955eb408de039571d5ca0cf010d350aed44
Nazrul Islam <nazrulislambhat@gmail.com>
2024-09-06 11:17:30 +0530

    +

--- src/components/DataFetch.jsx
+++ src/components/DataFetch.jsx
@@ -5,31 +5,43 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 export default function DataFetch() {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [error, setError] = useState();
+  const [page, setPage] = useState(0);
   useEffect(() => {
     const fetchPosts = async () => {
       setIsLoading(true);
-      const response = await fetch(`${BASE_URL}/posts`);
-      const posts = await response.json();
-      setPosts(posts);
-      setIsLoading(false);
+
+      try {
+        const response = await fetch(`${BASE_URL}/posts?page=${page}`);
+        const posts = await response.json();
+        setPosts(posts);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchPosts();
-  }, []);
+  }, [page]);
 
-  if (isLoading) {
-    return <h1>Loading...</h1>;
+  if (error) {
+    return <h1>Error: {error.message}</h1>;
   }
-
   return (
     <div>
       <h1>Data Fetching in React</h1>
-      <ul>
-        {posts.map((post) => {
-          return <li key={post.id}>{post.title}</li>;
-        })}
-      </ul>
+      <button onClick={() => setPage(page + 1)}>
+        Increased Page to {page}
+      </button>
+      {isLoading && <h1>Loading...</h1>}
+      {!isLoading && (
+        <ul>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 }
