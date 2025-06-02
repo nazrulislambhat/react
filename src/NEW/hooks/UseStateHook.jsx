00358d87feaf9217ed792835a40ba12a719d3181
Nazrul Islam <nazrulislambhat@gmail.com>
2025-06-02 07:41:17 +0530

    Some Changes

---
+++ src/NEW/hooks/UseStateHook.jsx
@@ -0,0 +1,27 @@
+import { pre } from 'motion/react-client';
+import { useState } from 'react';
+
+function UseStateHook() {
+  const [userDetails, setUserDetails] = useState({
+    name: 'John Doe',
+    age: 30,
+    email: 'user@email.com',
+  });
+  const changeName = () => {
+    setUserDetails((prevDetails) => ({
+      ...prevDetails,
+      name: 'nazrul',
+    }));
+  };
+  return (
+    <div>
+      <p>{userDetails.name}</p>
+      <p>{userDetails.age}</p>
+      <p>{userDetails.email}</p>
+
+      <button onClick={changeName}> Change Name</button>
+    </div>
+  );
+}
+
+export default UseStateHook;
