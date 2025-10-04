2cee56234ae6aceff393de8341370ec4fcf3741d
Nazrul Islam <nazrulislambhat@gmail.com>
2025-10-04 09:26:40 +0530

    Some Changes

---
+++ components/ButtonComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ButtonComponent() {
+  return <button>ButtonComponent</button>;
+}
+
+export default ButtonComponent;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import ButtonComponent from '../components/ButtonComponent';
+
 function App() {
-  return <h1>React</h1>;
+  return (
+    <>
+      <h1>React</h1>
+      <ButtonComponent />
+    </>
+  );
 }
 
 export default App;
