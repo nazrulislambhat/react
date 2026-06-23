5376b1aebb28c85faee41e6da4f0b2d80c7dbfb0
Nazrul Islam <nazrulislambhat@gmail.com>
2026-06-23 09:13:55 +0530

    1

--- autocomplete/hooks/UseDebounce.jsx
+++ autocomplete/hooks/UseDebounce.jsx
@@ -4,8 +4,8 @@ export function useDebounce(value, delay) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
-    const handler = setTimeout(() => setDebouncedValue(value), delay);
-    return () => clearTimeout(handler);
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timer);
   }, [value, delay]);
 
   return debouncedValue;
